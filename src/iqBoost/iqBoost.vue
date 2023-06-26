e72a7a3fe233beb372f957a484876d312746ce89
XzaiCloud <xzai0313@qq.com>
2023-06-27 00:51:10 +0800

    收录lowiq

--- src/iqBoost/iqBoost.vue
+++ src/iqBoost/iqBoost.vue
@@ -3,19 +3,19 @@
         <el-card class="box-card">
             <template #header>
                 <div class="card-header">
-                    <b>你可能需要 iqBoost</b>
-                    <el-button tag="a" href="//b8y.shop" target="_blank">求赞助</el-button>
+                    <b>欣赏LowIQ用户</b>
+                    <el-button tag="a" href="/">返回主页</el-button>
                 </div>
             </template>
             <div>
                 <!--主要内容-->
-                <el-image src="./index.webp" lazy/>
-                <h3>什么是 iqBoost？</h3>
-                <p>你是否感到你的智商一直在阻碍你的发展？你是否期望自己能变得更聪明、更有创造力、更成功？</p>
-                <p>现在，我们推出了 iqBoost 这款革命性的新产品可以帮助你释放全部潜力。我们的专有配方基于尖端研究，并已被证明在提高大脑功能方面非常有效。</p>
-                <p>最棒的是， iqBoost 非常容易使用。只需服用一粒胶囊，您就会很快感受到效果。无论是在备考考试、进行项目工作还是只是想提高认知能力， iqBoost 都是适合您的产品。</p>
-                <p>还在等什么？立即购买 iqBoost，释放您的全部潜力！</p>
-                <el-button tag="a" href="./iqBoost/">LowIQ用户大赏</el-button>
+                <h3>apple > android</h3>
+                <p/>
+                <el-image src="../LowIQ/1.jpg" lazy/>
+                <h3>亲妈献祭</h3>
+                <p/>
+                <el-image src="../LowIQ/2.webp" lazy/>
+                <p>如果你有素材，欢迎投稿xzai0313@qq.com</p>
                 <el-divider/>
                 <p><a id="hitokoto">
                     <el-icon class="is-loading">
@@ -26,7 +26,7 @@
                 &boxv;
                 <el-link type="primary" :underline="false" href="//b8y.top" target="_blank">Xzaiの个人主页</el-link>
                 <el-divider/>
-                <el-text>Create by <el-icon><ElementPlus/></el-icon> <el-link :underline="false" href="//b8y.top" target="_blank">Xzai</el-link>&boxv;纯属恶搞&boxv;<el-link type="primary" :underline="false" href="//beian.miit.gov.cn" target="_blank">蜀ICP备2022010825号-1</el-link></el-text>
+                <el-text>Create by <el-icon><ElementPlus/></el-icon> <el-link :underline="false" href="//b8y.top" target="_blank">Xzai</el-link>&boxv;<el-link type="primary" :underline="false" href="//beian.miit.gov.cn" target="_blank">蜀ICP备2022010825号-1</el-link></el-text>
                 <!--主要内容-->
             </div>
         </el-card>
